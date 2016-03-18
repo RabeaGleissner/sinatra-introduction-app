acf54b8c46f09d2069f5e065c612e9847b226114
Rabea Gleissner <rabeagleissner@gmail.com>
2016-03-18 10:05:48 +0000

    creates user journey and saves data in session

--- lib/introduction_app.rb
+++ lib/introduction_app.rb
@@ -2,24 +2,41 @@ require 'sinatra'
 require 'haml'
 
 class IntroductionApp < Sinatra::Base
+  enable :sessions
+
   get "/" do
     haml :home
   end
 
-  post "/country" do
-    @name = params[:name]
+  post "/name" do
+    session[:name] = params[:name]
+    redirect '/country'
+  end
+
+  get "/country" do
+    @name = session["name"]
     haml :country
   end
 
-  post "/animal" do
-    @country = params[:country]
+  post "/country" do
+    session[:country] = params[:country]
+    redirect '/animal'
+  end
+
+  get "/animal" do
+    @country = session["country"]
     haml :animal
   end
 
-  post "/summary" do
-    @name
-    @country
-    @animal = params[:animal]
+  post "/animal" do
+    session[:animal] = params[:animal]
+    redirect '/summary'
+  end
+
+  get "/summary" do
+    @name = session["name"]
+    @country = session["country"]
+    @animal = session["animal"]
     haml :summary
   end
 end
